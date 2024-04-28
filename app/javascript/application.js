0c3d9585b9f47a58847e28860e8af6615632ff82
Shravan <chaudharysharwan2000@gmail.com>
2024-04-28 19:50:23 +0530

    added user authentication

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "controllers"
+import "trix"
+import "@rails/actiontext"
+import "jquery"
+import "jquery_ujs"
+import 'popper'
+import 'bootstrap'
